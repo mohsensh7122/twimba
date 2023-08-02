88f6c4014ed7632d014e4049b69efed359357038
Mohsen Shirazi <mohsensh4792@gmail.com>
2023-08-02 14:15:30 -0400

    cleaned the code in index.js

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-import { tweetsData } from "./data.js";
-
-const tweetBtn = document.getElementById('tweet-btn');
-const tweetInput = document.getElementById('tweet-input');
-const feed = document.getElementById('feed');
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
 tweetBtn.addEventListener('click', function(){
-    console.log(tweetInput.value);
+    console.log(tweetInput.value)
 })
 
-
 function getFeedHtml(){
+    let feedHtml = ``
     
-    let feedHtml = ``;
-    tweetsData.forEach(tweet => {
-        feedHtml += `<div class="tweet">
-        <div class="tweet-inner">
-            <img src="${tweet.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${tweet.handle}</p>
-                <p class="tweet-text">${tweet.tweetText}</p>
-                <div class="tweet-details">
-                    <span class="tweet-detail">
-                        ${tweet.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.likes}
-                    </span>
-                    <span class="tweet-detail">
-                        ${tweet.retweets}
-                    </span>
-                </div>   
-            </div>            
-        </div>
-    </div>`
-    })
-
-    
-
-    return feedHtml;
-    
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
 }
 
-function renderTweets(){
-    feed.innerHTML = getFeedHtml();
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
 }
 
-renderTweets();
+render()
